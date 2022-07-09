dabd07a2b7c85fd617abf33d1f3307e8c4cb58ad
Jhamir15 <jhamir-jo@hotmail.com>
2022-07-08 22:46:50 -0500

    Quinto_Commit Detallando clase principal

--- src/main/java/com/mycompany/parcial/Asiento.java
+++ src/main/java/com/mycompany/parcial/Asiento.java
@@ -6,21 +6,26 @@ public class Asiento {
     private boolean estado;
     private float precio;
     
-    
+    enum estadoAsiento{
+        LIBRE, VENDIDO, INHABILITADO;
+    }
 
     public Asiento(int numero, float precio) {
         this.numero = numero;
         this.precio = precio;
+        this.estado = Boolean.FALSE;
+    }
+    public boolean isEstado() {
+        return this.estado;
     }
     
-   
     public boolean reservar(){
-        boolean result = false;
-        return result;
+        this.estado = Boolean.TRUE;
+        return this.estado;
     }
     
     public boolean cancelar(){
-        boolean result = false;
-        return result;
+        this.estado = Boolean.FALSE;
+        return this.estado;
     }
 }
--- src/main/java/com/mycompany/parcial/Parcial.java
+++ src/main/java/com/mycompany/parcial/Parcial.java
@@ -5,6 +5,16 @@ package com.mycompany.parcial;
 public class Parcial {
 
     public static void main(String[] args) {
-        
+        Cliente cliente_1 = new Cliente(100, 10, "12345678", "Jhamir");
+        Empleado empleado_1 = new Empleado(3500, 101, "23456789", "Diego");
+        Asiento asiento_1 = new Asiento(1, 200);
+        Venta venta_1 = new Venta();
+        System.out.println("Agregando cliente a la venta...");
+        System.out.println(venta_1.setCliente(cliente_1));
+        System.out.println("Agregando empleado a la venta...");
+        System.out.println(venta_1.setEmpleado(empleado_1));
+        System.out.println("Agregando asiento a la venta...");
+        System.out.println(venta_1.agregarAsiento(asiento_1));
+        System.out.println("\nAgregado exitosamente");
     }
 }
